6c8994faea8d60b8378f4f7a078502a19690f447
mirellaromero <mirellaromero228@gmail.com>
2025-04-03 11:03:01 -0500

    subida1

---
+++ ejer1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console - Write("Mi primera subida");
+        }
+    }
+}
